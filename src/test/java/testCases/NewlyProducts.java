2b50acbad9a9154081ac04c06e1136606a477779
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-06-01 18:43:02 +0530

    Update NewlyProducts.java

--- src/test/java/testCases/NewlyProducts.java
+++ src/test/java/testCases/NewlyProducts.java
@@ -10,8 +10,8 @@ public class NewlyProducts extends BaseClass {
 	@DataProvider
 	public Object[][] windowResolution() {
 
-		return new Object[][] { { 2560, 1440 }, { 1920, 1080 }, { 1280, 720 }, { 1920, 1200 }, { 1440, 900 },
-				{ 1366, 768 }, { 1366, 768 }, { 1680, 1050 },
+		return new Object[][] { //{ 2560, 1440 }, { 1920, 1080 },  { 1920, 1200 },{ 1680, 1050 }
+				{ 1366, 768 }, { 1280, 720 }, { 1440, 900 }, 
 				// { 1536, 864 } -getting 1.71
 		};
 	}
